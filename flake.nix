ef3f347c7f75455c24d21e233a65c71b8e628868
Mrugank Upadhyay <mrugank2@gmail.com>
2024-07-24 11:15:21 -0400

    Initial flake environment setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Dev shell provided via a flake.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Add packages here as required.
+          (python3.withPackages (p: with p; [
+            matplotlib
+          ]))
+        ];
+      };
+    });
+}
+
